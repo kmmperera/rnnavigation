1e8f54e852b49a852ce436fd50f448411385a52e
mahesh perera <maheshmadusankap@gmail.com>
2023-07-06 19:18:18 +0200

    beforecommittogithub

--- components/Profile.js
+++ components/Profile.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import {View,Text} from 'react-native';
+import Frontimg from "../assets/frontimg.svg";
 
  function Profile  (Props){
 
      return (
          <View >
              <Text>This is Profile</Text>
+             <Frontimg/>
          </View>
      );
  }
